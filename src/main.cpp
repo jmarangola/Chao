fbbc9228e901aaa9b710e91fe199add4a6c3525b
John Marangola <john.marangola@gmail.com>
2021-02-18 17:15:52 -0500

    initializing complementary filer

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "Wire.h" // For I2C communication
 #include <Arduino.h>
+#include <iomanip>
+#include <string>
+#include <math.h>
+
 
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050
 int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
@@ -7,15 +11,23 @@ int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
 double old_value, new_value = 0;
 double filtered = 0.0;
 
+// Complementary filter constants:
+const double imu_dt = 5; // 5 ms sample rate
+const double k_acc = 0.01;
+const double k_gyro = 0.01;
+
+// IMU variables:
+double pitch, roll;
+
 int16_t temperature; 
 char tmp_str[7]; 
 
-char* convert_int16_to_str(int16_t i) { // Converts int16 to string of uniform length
+char * convert_int16_to_str(int16_t i) { // Converts int16 to string of uniform length
   sprintf(tmp_str, "%6d", i);
   return tmp_str;
 }
 
-double get_raw_IMU_X(){
+double get_runnning_avg_IMU_X(){
     Wire.beginTransmission(MPU_ADDR);
     Wire.write(0x3B);
     Wire.endTransmission(false); 
@@ -32,8 +44,8 @@ double get_raw_IMU_X(){
 double n_sample_IMU_X(int n){ 
     double sample_sum;
     for (int i = 0; i < n; i++)
-        sample_sum += get_raw_IMU_X();
-    return (sample_sum / n)
+        sample_sum += get_runnning_avg_IMU_X();
+    return (sample_sum / n);
     
 }
 
@@ -48,6 +60,6 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(get_raw_IMU_X());
-  delay(50);
+  Serial.println(n_sample_IMU_X(50));
+  delay(1);
 }
