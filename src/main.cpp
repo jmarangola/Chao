46820cb1afd1097f0cacb8f49ec4bb69c1875edb
John Marangola <john.marangola@gmail.com>
2021-03-09 20:01:31 -0500

    Working input bluetooth ps4 controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "Wire.h" // For I2C communication
+/*#include "Wire.h" // For I2C communication
 #include <Arduino.h>
 #include <iomanip>
 #include <string>
@@ -62,4 +62,158 @@ void setup() {
 void loop() {
   Serial.println(n_sample_IMU_X(50));
   delay(1);
+}
+
+*/
+//#include <FastAccelStepper.h>
+//#include <PS4Controller.h>
+//using namespace std;
+
+// As in StepperDemo for Motor 1 on AVR
+//#define dirPinStepper    5
+//#define enablePinStepper 6
+//#define stepPinStepper   9  // OC1A in case of AVR
+
+
+
+/*
+#define stepPinStepper 21
+
+FastAccelStepperEngine engine = FastAccelStepperEngine();
+FastAccelStepper *stepper = NULL;
+
+
+void setup() {
+  // Initialize PS4 Controller:
+  Serial.begin(9600);
+  PS4.begin("03:03:03:03:03:03");
+  Serial.println("Ready.");
+
+  Serial.begin(9600);
+  engine.init();
+  stepper = engine.stepperConnectToPin(stepPinStepper);
+  if (stepper) {
+
+    // If auto enable/disable need delays, just add (one or both):
+    // stepper->setDelayToEnable(50);
+    // stepper->setDelayToDisable(1000);
+
+    stepper->setSpeed(500);  // the parameter is us/step !!!
+    stepper->setAcceleration(10000);
+    //stepper->move(5000);
+  }
+}
+
+void loop() {
+  if ( PS4.event.analog_move.stick.lx ) { 
+					Serial.print("Left Stick x at ");
+					Serial.println(PS4.data.analog.stick.lx, DEC);
+	}
+  delay(3000);
+}
+*/
+#include <Arduino.h>
+#include <PS4Controller.h>
+
+void setup()
+{
+    Serial.begin(9600);
+
+    PS4.begin("01:01:01:01:01:01");
+    Serial.println("Ready.");
+	Serial.println("test");
+}
+
+void loop()
+{
+		// Below has all accessible outputs from the controller
+    if(PS4.isConnected()) {
+			if ( PS4.data.button.up )
+					Serial.println("Up Button");
+			if ( PS4.data.button.down )
+					Serial.println("Down Button");
+			if ( PS4.data.button.left )
+					Serial.println("Left Button");
+			if ( PS4.data.button.right )
+					Serial.println("Right Button");
+				
+			if ( PS4.data.button.upright )
+					Serial.println("Up Right");
+			if ( PS4.data.button.upleft )
+					Serial.println("Up Left");
+			if ( PS4.data.button.downleft )
+					Serial.println("Down Left");
+			if ( PS4.data.button.downright )
+					Serial.println("Down Right");
+				
+			if ( PS4.data.button.triangle )
+					Serial.println("Triangle Button");
+			if ( PS4.data.button.circle )
+					Serial.println("Circle Button");
+			if ( PS4.data.button.cross )
+					Serial.println("Cross Button");
+			if ( PS4.data.button.square )
+					Serial.println("Square Button");
+				
+			if ( PS4.data.button.l1 )
+					Serial.println("l1 Button");
+			if ( PS4.data.button.r1 )
+					Serial.println("r1 Button");
+				
+			if ( PS4.data.button.l3 )
+					Serial.println("l3 Button");
+			if ( PS4.data.button.r3 )
+					Serial.println("r3 Button");
+				
+			if ( PS4.data.button.share )
+					Serial.println("Share Button");
+			if ( PS4.data.button.options )
+					Serial.println("Options Button");
+				
+			if ( PS4.data.button.ps )
+					Serial.println("PS Button");
+			if ( PS4.data.button.touchpad )
+					Serial.println("Touch Pad Button");
+				
+			if ( PS4.data.button.l2 ) {
+					Serial.print("l2 button at ");
+					Serial.println(PS4.data.analog.button.l2);
+			}
+			if ( PS4.data.button.r2 ) {
+					Serial.print("r2 button at ");
+					Serial.println(PS4.data.analog.button.r2, DEC);
+			}
+
+			if ( PS4.event.analog_move.stick.lx ) {
+					Serial.print("Left Stick x at ");
+					Serial.println(PS4.data.analog.stick.lx, DEC);
+			}
+			if ( PS4.event.analog_move.stick.ly ) {
+					Serial.print("Left Stick y at ");
+					Serial.println(PS4.data.analog.stick.ly, DEC);
+			}
+			if ( PS4.event.analog_move.stick.rx ) {
+					Serial.print("Right Stick x at ");
+					Serial.println(PS4.data.analog.stick.rx, DEC);
+			}
+			if ( PS4.event.analog_move.stick.ry ) {
+					Serial.print("Right Stick y at ");
+					Serial.println(PS4.data.analog.stick.ry, DEC);
+			}
+
+     if (PS4.data.status.charging)
+        Serial.println("The controller is charging");
+     if (PS4.data.status.audio)
+        Serial.println("The controller has headphones attached");
+     if (PS4.data.status.mic)
+        Serial.println("The controller has a mic attached");
+
+     Serial.print("Battey Percent : ");
+     Serial.println(PS4.data.status.battery, DEC);
+
+		 Serial.println();
+		 // This delay is to make the Serial Print more human readable
+		 // Remove it when you're not trying to see the output
+     delay(10);
+    }
 }
