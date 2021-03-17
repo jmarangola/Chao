8b86153d19a63cd7b601a35ba3877cfa7c08ea86
John Marangola <john.marangola@gmail.com>
2021-03-17 11:04:06 -0400

    computation of gyro angle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <MPU6050.h>
 #include <cmath>
 
+#define GYROSCOPE_S 65.6
+#define dT_MICROSECONDS 5000
+#define dT dT_MICROSECONDS/1000000.0
+
 MPU6050 IMU1;
 
 
@@ -12,8 +16,23 @@ void setup(){
   IMU1.setFullScaleGyroRange(MPU6050_GYRO_FS_500);
 }
 
-int16_t x, y, z;
+
+
+
+//float dT = 0.005;
+float deltaGyroAngle, accAngle;
+float gXSum = 0.0;
+int16_t gyrX, gyrY, gyrZ, aX, aY, aZ, oX, oY, oZ;
+
 void loop(){
-  IMU1.getRotation(&x, &y, &z);
-  Serial.println(y);
+  for (int i = 0; i <= 100; i++){
+    IMU1.getRotation(&oX, &oY, &oZ);
+    gXSum += oX;
+    delay(5);
+  }
+  gXSum /= 100;
+  IMU1.getMotion6(&gyrX, &gyrY, &gyrZ, &aX, &aY, &aZ);
+  accAngle = atan2f((float) aY, (float) aZ) * 180.0/(2.0*acos(0.0)) - 2;
+  deltaGyroAngle = ((float)((gyrX - gXSum)) / GYROSCOPE_S) * dT;
+  Serial.println(deltaGyroAngle);
 }
