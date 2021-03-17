ce6e7998164c15ca92bf3e96e21956c6d804d21b
John Marangola <john.marangola@gmail.com>
2021-03-17 12:24:02 -0400

    wFast accelstepper with high angular velocity

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <MPU6050.h>
 #include <cmath>
+#include <FastAccelStepper.h>
 
 #define GYROSCOPE_S 65.6
 #define dT_MICROSECONDS 5000
@@ -8,17 +9,41 @@
 #define STABLE_STATE_DELTA_THETA 0.05
 #define EMA_ALPHA 0.80
 
+#define STEP_LEFT_P 19
+#define DIR_LEFT__P 2
+#define STEP_RIGHT_P 0
+#define DIR RIGHT_P 18
+
+// IMU datum
 MPU6050 IMU1;
 float gyroscopeOffsets[3], thetaOld;
+
+// L/R Nema 17 Motors
+FastAccelStepperEngine engine = FastAccelStepperEngine();
+FastAccelStepper *stepperLeft = NULL;
+FastAccelStepper *stepperRight = NULL;
+
+void initMotors(){
+  engine.init();
+  // Initialize FastAccelStepper instances here:
+  stepperLeft = engine.stepperConnectToPin(STEP_LEFT_P);
+  //stepperRight = engine.stepperConnectToPin(STEP_RIGHT_P);
+  stepperLeft->setAutoEnable(true);
+  //stepperRight->setAutoEnable(true);
+}
 
 void setup(){
   Serial.begin(115200);
   Wire.begin(21,22,400000);
   IMU1.initialize();
   IMU1.setFullScaleGyroRange(MPU6050_GYRO_FS_500);
+  initMotors();
+  stepperLeft->setSpeed(400);  // the parameter is us/step !!!
+  stepperLeft->setAcceleration(50000);
+  stepperLeft->rampState();
+  stepperLeft->move(10000);
+
 }
-
-
 
 
 //float dT = 0.005;
@@ -48,8 +73,9 @@ void emaLowPass(float &accAngle, float &thetaPrimeFiltered, float thetaOld){
 
 float accAng = 0.0, thetaAngle = 0.0, thetOld = 0.0;
 void loop(){
-  computeGyroOffsets();
+  /*computeGyroOffsets();
   emaLowPass(accAngle, thetaAngle, thetOld);
   Serial.println(thetaAngle);
-  thetOld = thetaAngle;
+  thetOld = thetaAngle;*/
+  ;
 }
