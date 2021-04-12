074ede2951c54f6c5cb706e9daf5b2c849767029
John Marangola <john.marangola@gmail.com>
2021-04-12 17:09:45 -0400

    unfinished ramp for left motor with transition state

--- src/main.cpp
+++ src/main.cpp
@@ -76,20 +76,59 @@ void IRAM_ATTR leftTimerFunc(){
 }
 
 const int rampRate = 200;
-volatile int rEndV, rLevelV, rV=0;
+volatile int rEndV, rV=0;
 volatile int rSteps = 0;
 volatile int rState = 0;
 volatile int rDirection = 1; 
 volatile int rLastDirection = rDirection;
+volatile bool inTransition = 0;
 int rAbsoluteTime = 0.0;
 
 int dS = 20000;
 
 // 50,000 / speed  = time where speed is normalized between [0, 10] 
 // Trapezoidal motion profile
+/*
 void IRAM_ATTR rampLeftStepper() {
   portENTER_CRITICAL_ISR(&timerMux);
-    if (!rState) {
+  if (rSteps == dS) {
+    if (rDirection == rLastDirection) { // Same direction
+      if (rV == rEndV) {
+        
+      }
+      else if (rV < rEndV) { // Current speed is less than that of the desired setpoint, must ramp up a level
+        rAbsoluteTime = floor(50000.0/rV) - rampRate;
+        timerAlarmWrite(leftMotorTimer, rAbsoluteTime, true);
+        rV += floor(50000.0/rampRate);
+      }
+      else if (rV > rEndV) { // must ramp down
+        rAbsoluteTime = floor(50000.0/rV) + rampRate;
+        timerAlarmWrite(leftMotorTimer, rAbsoluteTime, true);
+        rV -= floor(50000.0/rampRate);
+      }
+      rSteps = 0;
+    }
+    else if (inTransition) { // Done decelerating, ready to accelerate to setpoint rEndV 
+      if (rV == 0) {
+        inTransition = false;
+        rV = 0;
+        rSteps = 0;
+      }
+      else { // Still decelerating
+        rAbsoluteTime = floor(50000.0/rV) + rampRate;
+        timerAlarmWrite(leftMotorTimer, rAbsoluteTime, true);
+        rSteps = 0;
+        rV -= floor(50000.0/rampRate);
+        rSteps++;
+      }
+    }
+  else { // Different direction, must ramp down then ramp up
+      digitalWrite(33, (rDirection==1) ? (LOW) : (HIGH));
+      inTransition = true;
+      rLastDirection = rDirection;
+    } 
+  }
+  if (!rState) {
     GPIO.out_w1ts = 1<< 19;
     rState = 1;
   }
@@ -97,33 +136,64 @@ void IRAM_ATTR rampLeftStepper() {
     rState = 0;
     GPIO.out_w1tc = 1<< 19;
   }
-  if (rSteps == dS) {
-    if (rDirection == rLastDirection) { // Same direction, ramp up
+  portEXIT_CRITICAL_ISR(&timerMux);
+}*/
+
+void IRAM_ATTR rampLeftStepper() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  if (rDirection == rLastDirection) {
+    if (rSteps == dS) { // Same direction
       if (rV == rEndV) {
-        rSteps = 0;
+        
       }
       else if (rV < rEndV) { // Current speed is less than that of the desired setpoint, must ramp up a level
         rAbsoluteTime = floor(50000.0/rV) - rampRate;
         timerAlarmWrite(leftMotorTimer, rAbsoluteTime, true);
-        rSteps = 0;
         rV += floor(50000.0/rampRate);
       }
       else if (rV > rEndV) { // must ramp down
         rAbsoluteTime = floor(50000.0/rV) + rampRate;
         timerAlarmWrite(leftMotorTimer, rAbsoluteTime, true);
+        rV -= floor(50000.0/rampRate);
+      }
+      rSteps = 0;
+    }
+    else if (inTransition && rSteps == dS) { // decelerating 
+      if (rV <= 0) { // finished decelerating
+        inTransition = false;
+        rV = 0;
+        rSteps = 0;
+      }
+      else { // Still decelerating
+        rAbsoluteTime = floor(50000.0/rV) + rampRate;
+        timerAlarmWrite(leftMotorTimer, rAbsoluteTime, true);
         rSteps = 0;
         rV -= floor(50000.0/rampRate);
       }
-      else {
-        Serial.println("Ramp control error.");
-      }
     }
-    else { // Different direction, must ramp down then ramp up
-
+    else { // direction is just changed
+      rSteps = dS;
+      inTransition = true;
+      digitalWrite(33, (rDirection==1) ? (LOW) : (HIGH));
     }
+  }
+  else { // Different direction, must ramp down then ramp up
+    digitalWrite(33, (rDirection==1) ? (LOW) : (HIGH));
+    inTransition = true;
+    rLastDirection = rDirection;
+  } 
+  if (!rState) {
+    GPIO.out_w1ts = 1<< 19;
+    rState = 1;
+  }
+  else {
+    rState = 0;
+    GPIO.out_w1tc = 1<< 19;
   }
   portEXIT_CRITICAL_ISR(&timerMux);
 }
+
+
 void IRAM_ATTR rightTimerFunc(){
   portENTER_CRITICAL_ISR(&timerMux);
   if (!state) {
